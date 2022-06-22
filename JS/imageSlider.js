d62906a69e302776a5c126ce7d56438c21612a29
Saulius <sauliusnesukaitis@gmail.com>
2022-06-22 19:01:20 +0300

    image slider

--- JS/imageSlider.js
+++ JS/imageSlider.js
@@ -1,43 +1,45 @@
-let images = ['./img/samsung1.png', './img/iphone-13.png', './img/iphone1.png', './img/pixel-6.png'];
+let images = ['./img/iphone1.png', './img/iphone-13.png',];
+let images2 = ['./img/iphone1.png', './img/iphone-13.png',];
+let images3 = ['./img/iphone1.png', './img/iphone-13.png',];
 
 
 // https://shop.ee.co.uk/gallery/pay-monthly/mobile-phones-h00001
 
 
 let index = 0;
-let index2 = 1;
-let index3 = 2;
+
 
 document.querySelector('.image').src = images[index];
-document.querySelector('.image2').src = images[index2];
-document.querySelector('.image3').src = images[index3];
+
 
 let next = document.getElementById('next-img');
-// let previous = document.getElementById('slider-previous');
+let previous = document.getElementById('previous-img');
 
 next.addEventListener('click', nextImage);
 
 function nextImage() {
     index+=1;
-    index2+=1;
-    index3+=1;
+
 
     if (index > images.length - 1) {
      index = 0;
     }
-    if (index2 > images.length - 1) {
-        index2 = 0;
-    }
-    if (index3 > images.length - 1) {
-        index3 = 0;
-    }
 
-     document.querySelector('.image').src = images[index];
-     document.querySelector('.image2').src = images[index2];
-     document.querySelector('.image3').src = images[index3];
+    document.querySelector('.image').src = images[index];
+
 }
 
+previous.addEventListener('click', previousImage);
 
+function previousImage(){
+    index-=1;
+    if (index < 0) {
+     index = images.length - 1;
+    }
+
+    document.querySelector('.image').src = images[index];
+
+}
 
 
 
